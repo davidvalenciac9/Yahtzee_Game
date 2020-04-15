da5ddffc4c8715564827604d33385f84d9099f8f
David Valencia <davidvalenciac9@gmail.com>
2020-04-15 17:26:43 -0400

    fixing toggleLocked

--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,22 @@
-import React, { Component } from "react";
-import "./Die.css";
+import React, {Component} from 'react';
+import './Die.css';
 
 class Die extends Component {
-  render() {
+  constructor (props) {
+    super (props);
+    this.handleClick = this.handleClick.bind (this);
+  }
+
+  handleClick () {
+    this.props.handleClick (this.props.idx);
+  }
+
+  render () {
     return (
       <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+        className={'Die'}
+        style={{backgroundColor: this.props.locked ? 'grey' : 'black'}}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
